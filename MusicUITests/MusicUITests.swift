6398bb5a42eea69ec3120bbea3e34dbd1b24f0a6
Malachi Hul <ts-malachi.hui@rakuten.com>
2020-05-30 07:29:02 +0900

    Initial Commit

---
+++ MusicUITests/MusicUITests.swift
@@ -0,0 +1,34 @@
+//
+//  MusicUITests.swift
+//  MusicUITests
+//
+//  Created by Hui, Malachi | DCMS on 2020/05/30.
+//  Copyright © 2020 Hui, Malachi | DCMS. All rights reserved.
+//
+
+import XCTest
+@testable import MusicUI
+
+class MusicUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
